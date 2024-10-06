391d09f1275aedad1f8fb11cbac024d4aba5596b
Ivanmartynenko-dev <martynenkoivan30042010@gmail.com>
2024-10-06 20:53:30 +0300

    add hello

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-print('hello')
+import telebot
+from telebot import types
+bot=telebot.TeleBot("7498922816:AAEVfH3FqfFvdw86CXw_QoqCj5qtAq09nOc")
+@bot.message_handler(commands=["start"])
+def Hello(message):
+    markup=types.InlineKeyboardMarkup()
+    btn1=types.InlineKeyboardButton(text="Базовая алгебра", url = 'https://en.wikipedia.org/wiki/Arithmetical_operation')
+    markup.add(btn1)
+    bot.send_message(message.from_user.id, "Приветствую! Я MathBoss. Попробуй пройти все мои уровни. Вся инфорция для их прохождения лежит тут", reply_markup = markup)
 
-a = 5
-b = 6
-print(a+b, 'result')
+
+
+bot.polling(none_stop=True, interval=0)
